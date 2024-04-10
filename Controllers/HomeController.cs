36622950f4067d58137359f865a967802bd2abcf
Sachin <sachin.sengar06@gmail.com>
2024-04-10 10:25:50 +0530

    Fist commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+      
+        [HttpGet]
+        public IActionResult get()
+        {
+            return Ok("Hi this is sample view");
+        }
+    }
+}
